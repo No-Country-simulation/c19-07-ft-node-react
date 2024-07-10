9541eb47c38f2789751a0d78d7ccafda681f27c1
MariaSolGonzalez <mariasolgonzalez240@hotmail.com>
2024-07-09 19:14:03 -0500

    created the first component for the parent user, which is their main view

--- client/src/modules/parents/PrincipalParentPage.tsx
+++ client/src/modules/parents/PrincipalParentPage.tsx
@@ -1,8 +1,8 @@
 
 export default function PrincipalParentPage() {
   return (
-    <div>
-      <h1>Hello Page</h1>
+    <div style={{ backgroundColor: "red" }}>
+    <h1>Hola aca estoy escribiendo un texto para ver cuanto se ve de el </h1>
     </div>
   );
 }
