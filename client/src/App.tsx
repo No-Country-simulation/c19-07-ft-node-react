94368928aed887a475c28f6b73cac1c024c303ce
jumagu <jmgg.gutierrez@gmail.com>
2024-07-10 17:41:57 -0500

    feat: Add protected route for private routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,40 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 
+import { ProtectedRoute } from "./components";
 import { HomePage, NotFoundPage } from "./pages";
-import LoginPage from "./modules/auth/pages/LoginPage.tsx";
+import { LoginPage, AuthLayout } from "./modules/auth/";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: (
+      <ProtectedRoute>
+        <HomePage />
+      </ProtectedRoute>
+    ),
     errorElement: <NotFoundPage />,
   },
   {
-    path: "/auth",
-    element: <LoginPage />,
-  },
-  {
-    path: "/dashboard",
+    path: "/auth/*",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "register",
+      },
+      {
+        path: "*",
+        element: <Navigate to="/auth/login" />,
+      },
+    ],
   },
 ]);
 
