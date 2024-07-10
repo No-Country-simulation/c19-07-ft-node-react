15994436c76affdfae83db64c8003664189a1068
MariaSolGonzalez <mariasolgonzalez240@hotmail.com>
2024-07-10 15:08:55 -0500

    resolution for rendering main content based on logged-in user

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 export function HomePage() {
   return (
     <>
-      <Box sx={{ display: "flex", overflowX: "hidden" }}>
+      <Box sx={{ display: "flex", flexDirection: "row", heigth:"100%"}}>
         <Drawer
           sx={{
             flexShrink: 0,
@@ -22,6 +22,8 @@ export function HomePage() {
                 lg: 240,
               },
               backgroundColor: "#abd1c6",
+              alignItems: "center",
+              gap:4
             },
           }}
           variant="permanent"
@@ -29,124 +31,237 @@ export function HomePage() {
         >
           <Box
             sx={{
-              height: "100%",
+              backgroundColor: "#D9D9D9",
+              width: "13vw",
+              height: "13vw",
+              borderRadius: "50%",
+              border: "1px solid black",
               display: "flex",
-              flexDirection: "column",
+              justifyContent: "center",
               alignItems: "center",
+              overflow: "hidden",
+              marginTop: 2,
             }}
           >
-            <Box
-              sx={{
-                backgroundColor: "#D9D9D9",
-                width: "15vw",
-                height: "15vw",
-                borderRadius: "50%",
-                border: "2px solid black",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                overflow: "hidden",
-                marginTop: 2,
-              }}
-            >
-              <img
-                src="IMG-ROUTE" //?PONER RUTA DE IMAGEN SEGUN LA VERIFICACION DE QUIEN ENTRO
-                alt="IMG-TEACHER-OR-STUDIENT" //?PONER UNA U OTRA DESCRIPCION SEGUN LA VERIFICACION DE QUIEN ENTRO
-                style={{ maxWidth: "100%", height: "auto" }}
-              />
-            </Box>
-
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-                marginTop: 5,
-                width: "100%",
-                gap: 5,
-              }}
-            >
-              <Button
-                sx={{
-                  backgroundColor: "#D9D9D9",
-                  color: "black",
-                  width: "100%",
-                  "&:hover": {
-                    backgroundColor: "#004643",
-                    color: "#abd1c6",
-                  },
-                }}
-              >
-                <p>Announcement</p>
-              </Button>
-              <Button
-                sx={{
-                  backgroundColor: "#D9D9D9",
-                  color: "black",
-                  width: "100%",
-                  "&:hover": {
-                    backgroundColor: "#004643",
-                    color: "#abd1c6",
-                  },
-                }}
-              >
-                <p>Classmates</p>
-              </Button>
-              <Button
-                sx={{
-                  backgroundColor: "#D9D9D9",
-                  color: "black",
-                  width: "100%",
-                  "&:hover": {
-                    backgroundColor: "#004643",
-                    color: "#abd1c6",
-                  },
-                }}
-              >
-                <p>Chat</p>
-              </Button>
-            </Box>
+            <img
+              src="IMG-ROUTE" //?PONER RUTA DE IMAGEN SEGUN LA VERIFICACION DE QUIEN ENTRO
+              alt="IMG-TEACHER-OR-STUDIENT" //?PONER UNA U OTRA DESCRIPCION SEGUN LA VERIFICACION DE QUIEN ENTRO
+              style={{ maxWidth: "100%", height: "auto" }}
+            />
           </Box>
+          <Button
+            sx={{
+              backgroundColor: "#D9D9D9",
+              color: "black",
+              width: "100%",
+              "&:hover": {
+                backgroundColor: "#004643",
+                color: "#abd1c6",
+              },
+            }}
+          >
+            <p>Announcement</p>
+          </Button>
+          <Button
+            sx={{
+              backgroundColor: "#D9D9D9",
+              color: "black",
+              width: "100%",
+              "&:hover": {
+                backgroundColor: "#004643",
+                color: "#abd1c6",
+              },
+            }}
+          >
+            <p>Classmates</p>
+          </Button>
+          <Button
+            sx={{
+              backgroundColor: "#D9D9D9",
+              color: "black",
+              width: "100%",
+              "&:hover": {
+                backgroundColor: "#004643",
+                color: "#abd1c6",
+              },
+            }}
+          >
+            <p>Chat</p>
+          </Button>
         </Drawer>
-
-        <AppBar
-          position="static"
+        <Box
           sx={{
-            flexGrow: 1,
-            padding: "10px",
-            marginLeft: 0,
-            backgroundColor: "#abd1c6",
+            display: "flex",
+            flexDirection: "column",
+            backgroundColor: "purple",
+            width: "100%",
           }}
         >
-          <Box
+          <AppBar
+            position="static"
             sx={{
-              width: "81%",
-              display: "flex",
-              justifyContent: "space-between",
-              flexDirection: "row",
+              flexGrow: 1,
+              padding: "20px",
+              marginLeft: 0,
+              backgroundColor: "#abd1c6",
             }}
           >
-            <p>School Name</p>
-            <p>Name of teacher/student</p> {/*ESTO TIENE QUE VARIAR SEGUN LA VERIFICACION */}
+            NAVBAR
+          </AppBar>
+          <Box sx={{
+            flexGrow: 1,
+            minHeight: 0, 
+            backgroundColor: "#004643",
+            padding: 3,
+            }}>
+            <Outlet />
           </Box>
-        </AppBar>
-      </Box>
-
-      <Box 
-        position="static"
-        sx={{
-          flexGrow: 1,
-          padding: "10px",
-          marginLeft: 0,
-          backgroundColor: "purple",
-        }}
-      > {/*COMO HAGO PARA QUE ESTO SE PONGA EN PARALELO AL SIDEBAR Y NO POR DEBAJO DE EL*/}
-        <p>
-          SOL1 SOL2 SOL3 SOL4 SOL5 SOL6 SOL7 SOL8 SOL9 SOL10
-        </p>
-        <Outlet />
+        </Box>
       </Box>
     </>
   );
 }
+
+// <Box sx={{ display: "flex", overflowX: "hidden" }}>
+//   <Drawer
+//     sx={{
+//       flexShrink: 0,
+//       width: {
+//         xs: 150,
+//         sm: "25%",
+//         md: 240,
+//         lg: 240,
+//       },
+//       "& .MuiDrawer-paper": {
+//         width: {
+//           xs: 150,
+//           sm: "25%",
+//           md: 240,
+//           lg: 240,
+//         },
+//         backgroundColor: "#abd1c6",
+//       },
+//     }}
+//     variant="permanent"
+//     anchor="left"
+//   >
+//     <Box
+//       sx={{
+//         height: "100%",
+//         display: "flex",
+//         flexDirection: "column",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Box
+//         sx={{
+//           backgroundColor: "#D9D9D9",
+//           width: "15vw",
+//           height: "15vw",
+//           borderRadius: "50%",
+//           border: "2px solid black",
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//           overflow: "hidden",
+//           marginTop: 2,
+//         }}
+//       >
+//         <img
+//           src="IMG-ROUTE" //?PONER RUTA DE IMAGEN SEGUN LA VERIFICACION DE QUIEN ENTRO
+//           alt="IMG-TEACHER-OR-STUDIENT" //?PONER UNA U OTRA DESCRIPCION SEGUN LA VERIFICACION DE QUIEN ENTRO
+//           style={{ maxWidth: "100%", height: "auto" }}
+//         />
+//       </Box>
+
+//       <Box
+//         sx={{
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//           flexDirection: "column",
+//           marginTop: 5,
+//           width: "100%",
+//           gap: 5,
+//         }}
+//       >
+//         <Button
+//           sx={{
+//             backgroundColor: "#D9D9D9",
+//             color: "black",
+//             width: "100%",
+//             "&:hover": {
+//               backgroundColor: "#004643",
+//               color: "#abd1c6",
+//             },
+//           }}
+//         >
+//           <p>Announcement</p>
+//         </Button>
+//         <Button
+//           sx={{
+//             backgroundColor: "#D9D9D9",
+//             color: "black",
+//             width: "100%",
+//             "&:hover": {
+//               backgroundColor: "#004643",
+//               color: "#abd1c6",
+//             },
+//           }}
+//         >
+//           <p>Classmates</p>
+//         </Button>
+//         <Button
+//           sx={{
+//             backgroundColor: "#D9D9D9",
+//             color: "black",
+//             width: "100%",
+//             "&:hover": {
+//               backgroundColor: "#004643",
+//               color: "#abd1c6",
+//             },
+//           }}
+//         >
+//           <p>Chat</p>
+//         </Button>
+//       </Box>
+//     </Box>
+//   </Drawer>
+
+//   <AppBar
+//     position="static"
+//     sx={{
+//       flexGrow: 1,
+//       padding: "10px",
+//       marginLeft: 0,
+//       backgroundColor: "#abd1c6",
+//     }}
+//   >
+//     <Box
+//       sx={{
+//         width: "81%",
+//         display: "flex",
+//         justifyContent: "space-between",
+//         flexDirection: "row",
+//       }}
+//     >
+//       <p>School Name</p>
+//       <p>Name of teacher/student</p> {/*ESTO TIENE QUE VARIAR SEGUN LA VERIFICACION */}
+//     </Box>
+//   </AppBar>
+// </Box>
+
+// <Box
+//   position="static"
+//   sx={{
+//     flexGrow: 1,
+//     padding: "10px",
+//     marginLeft: 0,
+//     backgroundColor: "purple",
+//   }}
+// > {/*COMO HAGO PARA QUE ESTO SE PONGA EN PARALELO AL SIDEBAR Y NO POR DEBAJO DE EL*/}
+//   <p>
+//     SOL1 SOL2 SOL3 SOL4 SOL5 SOL6 SOL7 SOL8 SOL9 SOL10
+//   </p>
+//   <Outlet />
+// </Box>
