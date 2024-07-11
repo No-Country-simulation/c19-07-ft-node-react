7a1f9eae4e673e726269e96c65e05f9e4913a098
Rama <ramirocarnicersouble8@gmail.com>
2024-07-10 23:12:55 -0300

    [feat] Seeder for courses

--- server/prisma/seed/academicRecords.seeder.ts
+++ server/prisma/seed/academicRecords.seeder.ts
@@ -28,6 +28,22 @@ interface IEvaluations {
   date: Date
 }
 
+interface IProfessors {
+  professor_id: string
+  user_id: string
+  area_academica_id: string
+  fecha_contratacion: string
+  estado_empleado: string
+  educational_level_id: string
+  createdAt: Date
+  updatedAt: Date
+}
+interface IAcademicAreas {
+  area_academica_id: string
+  nombre: string
+  descripcion: string
+  nivel_educativo: string
+}
 const prisma = new PrismaClient()
 
 const seedAcademicRecords = async (): Promise<void> => {
@@ -38,7 +54,7 @@ const seedAcademicRecords = async (): Promise<void> => {
     await prisma.academic_records.create({
       data: {
         student_id: student.student_id,
-        curso_id: courses[i].cursos_id,
+        curso_id: courses[faker.number.int({ min: 0, max: courses.length - 1 })].cursos_id,
         mark: faker.number.float({ max: 100, min: 100, fractionDigits: 1 }),
         comment: faker.lorem.words({ min: 4, max: 10 }),
         date: new Date()
@@ -70,7 +86,7 @@ const seedEvaluationsResult = async (): Promise<void> => {
     await prisma.evaluation_results.create({
       data: {
         evaluation_id: e.evaluation_id,
-        student_id: students[i].student_id,
+        student_id: students[faker.number.int({ min: 0, max: students.length - 1 })].student_id,
         mark: faker.number.float({ min: 0, max: 100, fractionDigits: 1 }),
         comment: faker.lorem.words({ min: 4, max: 10 })
       }
@@ -78,7 +94,24 @@ const seedEvaluationsResult = async (): Promise<void> => {
   })
 }
 
+const seedCourses = async () => {
+  const professors: IProfessors[] = await prisma.professors.findMany()
+  const academicAreas: IAcademicAreas[] = await prisma.academic_areas.findMany()
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  professors.forEach(async (profesor: IProfessors) => {
+    await prisma.courses.create({
+      data: {
+        nombre: faker.person.jobArea(),
+        descripcion: faker.lorem.words({ min: 4, max: 10 }),
+        professor_id: profesor.professor_id,
+        area_academica_id: academicAreas[faker.number.int({ min: 0, max: academicAreas.length - 1 })].area_academica_id
+      }
+    })
+  })
+}
+
 export const mainAcademicRecords = async (): Promise<void> => {
+  await seedCourses()
   await seedAcademicRecords()
   await seedEvaluations()
   await seedEvaluationsResult()
