63a484507422db8c4940cdb8b820e927fa4d7760
Rama <ramirocarnicersouble8@gmail.com>
2024-07-12 20:59:44 -0300

    [fix] Port Undefined when there is not env variable port

--- server/src/app.ts
+++ server/src/app.ts
@@ -23,7 +23,7 @@ class Server {
   }
 
   config () {
-    this.app.set('port', process.env.PORT_SERVER !== null ? process.env.PORT_SERVER : 3000)
+    this.app.set('port', process.env.PORT_SERVER !== undefined ? process.env.PORT_SERVER : 3000)
     this.app.use(morgan('dev'))
     this.app.use(express.json())
     this.app.use(cookieParser())
