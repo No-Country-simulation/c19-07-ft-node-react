62d856baa7ec21cdd3915d47e80eaf2d2cd81aa9
Rama <ramirocarnicersouble8@gmail.com>
2024-07-18 18:58:23 -0300

    [feat] Interface and Types for Evaluations and Evaluations result creation added

--- server/src/types/professors.type.ts
+++ server/src/types/professors.type.ts
@@ -5,3 +5,16 @@ export interface CreateProfessor {
   employeeState: string
   userId: string
 }
+export interface CreateEvaluationAndResults {
+  curso_id: string
+  name: string
+  description: string
+  date: Date
+  student_id: string
+  mark: number
+  comment: string
+}
+
+export type CreateEvaluations = Omit<CreateEvaluationAndResults, 'student_id' | 'mark' | 'comment' | 'curso_id'>
+
+export type CreateEvaluationResult = Omit<CreateEvaluationAndResults, 'curso_id' | 'name' | 'description' | 'date' >
