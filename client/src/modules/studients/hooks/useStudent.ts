e6ba10fcecd761c1b86d807b23e651c4f690c547
MariaSolGonzalez <mariasolgonzalez240@hotmail.com>
2024-07-24 14:56:24 -0500

    uncoment the fetch for the api call to get student info

--- client/src/modules/studients/hooks/useStudent.ts
+++ client/src/modules/studients/hooks/useStudent.ts
@@ -1,23 +1,23 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import { useAxiosPrivate } from "../../../hooks";
-
+import { Student } from "../../../interfaces";
 
 export const useStudent = () => {
-    const api = useAxiosPrivate()
+  const api = useAxiosPrivate();
 
-
-    const [students, setStudents] = useState<any[]>([]);
+  const [students, setStudents] = useState<any[]>([]);
 
   const fetchStudents = async () => {
+    
     try {
-      const response = await api.get(`/students`);
+      const response = await api.get(`/parents/details`);
       setStudents(response.data);
+      console.log(students);      
     } catch (error) {
       console.error("Error fetching students:", error);
     }
   };
-
 
   useEffect(() => {
     fetchStudents();
@@ -25,6 +25,6 @@ export const useStudent = () => {
 
   return {
     fetchStudents,
-    students
-  }
+    students,
+  };
 };
