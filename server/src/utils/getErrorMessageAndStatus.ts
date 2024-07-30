7ed82bc9a51d4080ac4a013c7c56cbace5c1693e
Rama <ramirocarnicersouble8@gmail.com>
2024-07-30 11:29:42 -0300

    [feat] Validation custom error added

--- server/src/utils/getErrorMessageAndStatus.ts
+++ server/src/utils/getErrorMessageAndStatus.ts
@@ -1,7 +1,9 @@
 import { DatabaseError } from '../errors/databaseError'
+import { ValidationError } from '../errors/validationError'
 import { ErrorMessageAndStatus } from '../types/error.type'
 
 export const getErrorMessageAndStatus = (error: Error): ErrorMessageAndStatus => {
   if (error instanceof DatabaseError) return { message: 'A Database Error ocurred', status: 503 }
+  if (error instanceof ValidationError) return { message: 'The body has an invalid property', status: 400 }
   return { message: 'Server internal error, error unknown', status: 500 }
 }
